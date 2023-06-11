50d3105622e4d337839195bfe87a2a0d808978c1
kavishkamk <kavishkamadhushan727@gmail.com>
2023-06-11 13:00:02 +0530

    setup marketing app

---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const mount = (el) => {
+    ReactDOM.render(
+        <App />, el);
+};
+
+// If we are in development and in isolation,
+// call mount immediately
+if (process.env.NODE_ENV === 'development') {
+
+    const devRoot = document.querySelector('#marketing-dev-root');
+
+    if (devRoot) {
+        mount(devRoot);
+    }
+}
+
+export { mount };
