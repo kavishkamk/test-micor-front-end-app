57ba5fd1726975f00d5d162e1a5ac46ddad7f548
kavishkamk <kavishkamadhushan727@gmail.com>
2023-07-08 13:08:07 +0530

    add simple auth flow

--- container/src/App.js
+++ container/src/App.js
@@ -1,23 +1,29 @@
-import React, {lazy, Suspense} from "react";
+import React, {lazy, Suspense, useState} from "react";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
 const AuthLazy = lazy(() => import("./components/AuthApp"));
 const MarketingLazy = lazy(() => import("./components/MarketingApp"));
 import Header from "./components/Header";
+import Progress from "./components/Progress";
 
 const generateClassName = createGenerateClassName({
     productionPrefix: "co",
 });
 
 export default () => {
+
+    const [isSignedIn, setIsSignedIn] = useState(false);
+
     return (
         <BrowserRouter>
             <StylesProvider generateClassName={generateClassName}> 
-                <Header/>
-                <Suspense fallback={<div>Loading...</div>}>
+                <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)}/>
+                <Suspense fallback={<Progress />}>
                     <Switch>
-                        <Route path="/auth" component={AuthLazy} />
+                        <Route path="/auth">
+                            <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+                        </Route>
                         <Route path="/" component={MarketingLazy} />
                     </Switch>
                 </Suspense>
--- container/src/components/AuthApp.js
+++ container/src/components/AuthApp.js
@@ -3,7 +3,7 @@ import { useHistory } from "react-router-dom";
 
 import {mount} from "auth/AuthApp";
 
-export default () => {
+export default ({onSignIn}) => {
 
     const ref = useRef(null);
     const history = useHistory();
@@ -17,7 +17,8 @@ export default () => {
                 if(pathname !== nextPathname) {
                     history.push(nextPathname);
                 }
-            }
+            },
+            onSignIn
         });
 
         history.listen(onParentNavigate);
